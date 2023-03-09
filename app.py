9d965dacba8c2a1736d9336e963cf8233d41d7e6
David Pratap <david.pratap@greatlearning.in>
2023-03-09 21:00:12 +0530

    first commit

--- app.py
+++ app.py
@@ -1,44 +1,42 @@
 import streamlit as st
-st.title("Medical Diagnostic Web App")
 import pandas as pd
-import numpy as np
+from sklearn.preprocessing import StandardScaler
 import pickle
+st.title("Medical Diagnostics Web App")
+st.subheader("Is the candidate diabetic?")
 
-st.subheader("Is the person diabetic or not?")
-# Step1 : Load the picked pipeline
-model=open("rfc.pickle", 'rb')
+# Step1: Load the model
+model=open('rfc.pickle', 'rb')
 clf=pickle.load(model)
 model.close()
 
-
-# Step2: Get the input from the front end user
+# Step2 : Get the front end user inout
 pregs=st.number_input('Pregnancies',0,20,0)
-glucose=st.slider('Glucose',44,199,44)
-bp=st.slider('BloodPressure',20,140,20) 
-skin=st.slider('SkinThickness',7.0,99.0,7.0)
-insulin=st.slider('Insulin',10, 900,10)
-bmi=st.slider('BMI',15, 70,15)
-dpf=st.slider('DiabetesPedigreeFunction',0.50, 2.50,0.05)
-age=st.slider('Age',20, 90, 20)
+glucose=st.slider('Glucose',40.0, 200.0, 40.0)
+bp=st.slider('BloodPressure',20.0, 140.0, 20.0)
+skin=st.slider('SkinThickness',7.0, 99.0, 7.0)
+insulin=st.slider('Insulin',14.0, 850.0, 14.0)
+bmi=st.slider('BMI',18.0, 70.0, 18.0)
+dpf=st.slider('DiabetesPedigreeFunction',0.05, 2.50, 0.05)
+age=st.slider('Age', 20, 85, 20)
 
-# Step3: Get the model input
+# Step3: Get the inout as model input
 data={
-    'Pregnancies':pregs,
+    'Pregnancies':pregs, 
     'Glucose':glucose, 
-    'BloodPressure':bp,
+    'BloodPressure':bp, 
     'SkinThickness':skin, 
     'Insulin':insulin,
     'BMI':bmi, 
-    'DiabetesPedigreeFunction':dpf, 
-    'Age':age}
-
+    'DiabetesPedigreeFunction':dpf,
+    'Age':age
+}
 input_data=pd.DataFrame([data])
 
-# Step4: Get the prediction and print the result
-prediction=clf.predict(input_data)[0]
-st.write(prediction)
+# Step4 : Get the predictions and print result
+preds=clf.predict(input_data)[0]
 if st.button("Predict"):
-    if prediction==1:
-        st.error("Diabetic")
-    if prediction==0:
-        st.success("Healthy")
+    if preds==1:
+        st.subheader("Diabetic")
+    if preds==0:
+        st.subheader("Non-diabetic")
